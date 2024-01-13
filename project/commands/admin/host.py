c659847c06b31d157a589ca109e7be943be8f0b2
simon <155122848+simonpotel@users.noreply.github.com>
2024-01-13 20:05:32 +0100

    add host infos
    Added more informations about the host on !host command:
    + Processor infos
    + Disks infos
    + IP Local info
    + current_working_directory

--- project/commands/admin/host.py
+++ project/commands/admin/host.py
@@ -1,31 +1,58 @@
 import discord
 import platform
 import psutil
+import socket
 from ...misc.logs import *
 
 async def host(client, message, args):
     try:
+        # Informations système
         system_info = platform.system()
         node_name = platform.node()
         release_info = platform.release()
         version_info = platform.version()
         machine_info = platform.machine()
         processor_info = platform.processor()
-        cpu_info = platform.processor()
 
+        # Informations CPU
+        cpu_info = f"**Processor:** {processor_info}\n"
+        cpu_info += f"**Physical Cores:** {psutil.cpu_count(logical=False)}\n"
+        cpu_info += f"**Total Cores:** {psutil.cpu_count(logical=True)}"
+
+        # Informations mémoire
         memory_info = psutil.virtual_memory()
-        disk_info = psutil.disk_usage('/')
 
+        # Informations disque
+        disk_info = psutil.disk_usage('/')
+        disk_usage_info = f"**Disk Usage:** {disk_info.percent}% used\n"
+        disk_usage_info += f"**Total Disk Space:** {round(disk_info.total / (1024 ** 3), 2)} GB\n"
+        disk_usage_info += f"**Used Disk Space:** {round(disk_info.used / (1024 ** 3), 2)} GB\n"
+        disk_usage_info += f"**Free Disk Space:** {round(disk_info.free / (1024 ** 3), 2)} GB"
+
+        # Informations sur la connexion Internet
+        try:
+            host_name = socket.gethostname()
+            host_ip = socket.gethostbyname(host_name)
+            internet_info = f"**Host Name:** {host_name}\n"
+            internet_info += f"**IP Address:** {host_ip}"
+        except socket.error:
+            internet_info = "Unable to retrieve Internet information."
+
+        # Répertoire de travail du bot
+        current_working_directory = f"**Current Working Directory:** {os.getcwd()}"
+
+        # Description de l'embed
         embed_description = (
             f"**System:** {system_info}\n"
             f"**Node Name:** {node_name}\n"
             f"**Release:** {release_info}\n"
             f"**Version:** {version_info}\n"
             f"**Machine:** {machine_info}\n"
-            f"**Processor:** {processor_info}\n"
-            f"**CPU:** {cpu_info}\n"
+            f"{cpu_info}\n"
             f"**Memory Usage:** {memory_info.percent}% used\n"
-            f"**Disk Usage:** {disk_info.percent}% used"
+            f"{disk_usage_info}\n"
+            f"{internet_info}\n"
+            f"{current_working_directory}"
         )
 
         await send_embed(
