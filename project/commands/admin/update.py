0be48276b5e58cf52a52855726bd24d1a0adb629
SMN_ <155122848+simonpotel@users.noreply.github.com>
2024-02-03 09:44:31 +0100

    add git submodule update on update command

--- project/commands/admin/update.py
+++ project/commands/admin/update.py
@@ -12,14 +12,38 @@ async def update(client, message, args):
     if len(args) != 1: return await error(message.channel, "Invalid argument. Use `update <project>`")
     if args[0] in config:
         try:
-            result = subprocess.run(['git', '-C', config[args[0]], 'pull'], capture_output=True, text=True)
-            await send_embed(
-                message.channel,
-                "🖥️ git update",
-                result,
-                discord.Color.pink()
-            )
+            project_path = config[args[0]]
+
+            # Git pull the main project
+            pull_result = subprocess.run(['git', '-C', project_path, 'pull'], capture_output=True, text=True)
+
+            # Check if the git pull was successful
+            if pull_result.returncode == 0:
+                await send_embed(
+                    message.channel,
+                    "🖥️ git pull",
+                    pull_result,
+                    discord.Color.pink()
+                )
+
+                # Git submodule update --remote
+                submodule_result = subprocess.run(['git', '-C', project_path, 'submodule', 'update', '--remote'], capture_output=True, text=True)
+
+                if submodule_result.returncode == 0:
+                    await send_embed(
+                        message.channel,
+                        "🖥️ git submodule update",
+                        submodule_result,
+                        discord.Color.pink()
+                    )
+                else:
+                    await error(message.channel, f"Error during submodule update: {submodule_result.stderr}")
+
+            else:
+                await error(message.channel, f"Error during command ['git', '-C', {project_path}, 'pull']\n {pull_result.stderr}")
+
         except Exception as e:
-                await error(message.channel, f"Error during command ['git', '-C', {config[args[0]]}, 'pull']\n {e}")
+            await error(message.channel, f"An unexpected error occurred: {e}")
+
     else:
          return await error(message.channel, "Invalid argument. Use `update <project>` with a project configured in update_config.json")
