8aebc0e836cb3ff8568ad37e909f3898b994fd86
simon <155122848+simonpotel@users.noreply.github.com>
2024-02-11 17:53:23 +0100

    + log_bot_infos design

--- project/log_bot_infos.py
+++ project/log_bot_infos.py
@@ -12,34 +12,35 @@ import re
 
 def escape_special_chars(string):
     # Escape underscores, asterisks, and double underscores
-    string = re.sub(r'[_*]', r'\\\g<0>', string)
+    string = re.sub(r'([\\*_])', r'\\\1', string)
     return string
 
-async def log_bot_info(client):
+async def log_bot_infos(client):
     try:
+        # Obtention des informations sur le bot
         bot_config = get_bot_config()
         guild = client.get_guild(int(bot_config["discordid"]))
         if "logschannelid" not in bot_config:
             return
-
         channel = guild.get_channel(int(bot_config["logschannelid"]))
 
-        # Host information
+        # Informations sur l'hôte
         external_ip = get_external_ip()
         local_ip = socket.gethostbyname(socket.gethostname())
         boot_time = psutil.boot_time()
         uptime_seconds = int(time.time() - boot_time)
         uptime_str = format_time(uptime_seconds)
 
-        # Get Git information
+        # Informations sur Git
         git_branch = subprocess.check_output(['git', 'rev-parse', '--abbrev-ref', 'HEAD']).decode().strip()
         git_branch = escape_special_chars(git_branch)
         last_commits = subprocess.check_output(['git', 'log', '-10', '--pretty=format:%H %an %s']).decode().strip().split('\n')
 
-        # Get the repository URL
+        # URL du dépôt Git
         repository_url = subprocess.check_output(['git', 'config', '--get', 'remote.origin.url']).decode().strip().replace('.git', '')
 
         embed_description = (
+            f"\n"
             f"**🖥️ Host**\n"
             f"**External IP Address:** {external_ip}\n"
             f"**Local IP Address:** {local_ip}\n"
@@ -56,9 +57,9 @@ async def log_bot_info(client):
             commit_hash = commit_parts[0]
             commit_author = commit_parts[1]
             commit_message = commit_parts[2]
-            commit_url = f"{repository_url}/commit/{commit_hash}"
-            commit_author = escape_special_chars(commit_author)
-            embed_description += f"**{commit_author}** : {commit_message} [🔗]({commit_url})\n"
+            commit_url = f"[{commit_message}]({repository_url}/commit/{commit_hash})"
+            commit_message = escape_special_chars(commit_message)
+            embed_description += f"**{commit_author}**  :  {commit_url}\n"
 
         await discord_log(client, "🤖 Bot started", embed_description)
 
