a834d3d1d2c11ae92b8d3f999d21d43b5642179a
simon <potelsmn@gmail.com>
2024-02-26 14:30:08 +0100

    config.py comments

--- discord_bot/config.py
+++ discord_bot/config.py
@@ -1,6 +1,11 @@
 from DEXcryptoLib.Lib.Misc.json import get_json_content, write_json
 
 def get_config(file_path, default_config):
+    """
+    Return dic {} 
+    of the file_path file
+    and if the file dont exist, write the default content default_config
+    """
     config = get_json_content(file_path)
     if config == -1:
         write_json(file_path, default_config)
@@ -9,10 +14,23 @@ def get_config(file_path, default_config):
     return config
 
 def get_bot_config():
-    return get_config("configs/bot_config.json", {'token': '0x0', 'prefix': '$', 'discordid': '-1', 'logschannelid': '-1', 'roomscategid': '-1'})
+    return get_config(
+        "configs/bot_config.json", 
+        {'token': '0x0', 
+         'prefix': '$', 
+         'discordid': '-1', 
+         'logschannelid': '-1', 
+         'roomscategid': '-1'}
+        )
 
 def get_git_config():
-    return get_config("configs/git_config.json", {})
+    return get_config(
+        "configs/git_config.json", 
+        {}
+        )
 
 def get_tmux_config():
-    return get_config("configs/tmux_config.json", {'example': 'color a'})
+    return get_config(
+        "configs/tmux_config.json", 
+        {'': ''}
+        )
