d0eb148eb6c52274dd1a9032901c3d42bb95ac6d
simon <potelsmn@gmail.com>
2024-02-26 18:27:07 +0100

    error comments

--- discord_bot/discordlogs/error.py
+++ discord_bot/discordlogs/error.py
@@ -1,6 +1,14 @@
 import discord 
 from discord_bot.embeds.embeds import send_embed
+from .discord_log import discord_log
 
 async def error(message, error):
-    if not message: return 
+    """
+    Sends an error embed in the channel.
+    """
+    # Check if message is None to avoid errors when called internally
+    if not message:
+        return
+
+    # Send the error embed
     await send_embed(message, "❌ Error", error, discord.Color.brand_red())
