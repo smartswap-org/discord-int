1341f3e7e2c72bdeade01ccd5f8f8fce8374557c
simon <potelsmn@gmail.com>
2024-02-26 14:07:21 +0100

    bot.py comments

--- bot.py
+++ bot.py
@@ -3,19 +3,20 @@ from discord import Activity, ActivityType
 from discord.ext import tasks
 from discord_bot import * # Project includes
 
-bot_config = get_bot_config()
+#To make: a script to install all the dependicies with pip
+    #dependicies:
+    #web3
+    #discord
+    #pmysql-connector-python
+    #psutil
+
+bot_config = get_bot_config() # Get the config configs/bot_config.json
 
 client_commands = {
-
+    # No commands yet for clients
 }
 
-#dependicies:
-#web3
-#discord
-#pmysql-connector-python
-#psutil
-
-admin_commands = { # All bot commands
+admin_commands = { # Admin commands: discord_bot/commands/admin/*
     'clear': clear,
     'host': host,
     'restart': restart,
@@ -43,38 +44,58 @@ async def help(client, message, args): # Command !help that print all commands
 
 client_commands["help"] = help
 
-
-class MyClient(discord.Client): # Create the client object for the bot
-    async def on_ready(self): # When the bot start
+class MyClient(discord.Client):
+    async def on_ready(self): 
+        """
+        Toggeled at every start of the bot
+        """
         subprocess.run(["bash", "scripts/create_config_files.sh"]) # Create config scripts if they dont exists
-        print(f'Logged in as {self.user} (ID: {self.user.id})')
-        self.db_smartswap = init_database("smartswap", "configs/db_config.json")
-        self.db_smartswap.connect()
+        print(f'Logged in as {self.user} (ID: {self.user.id})') 
+        self.db_smartswap = init_database("smartswap", "configs/db_config.json") # Get config of the database smartswap from configs/db_config.json
+        self.db_smartswap.connect() # Connected to the database smartswap
         await self.change_presence(activity=Activity(type=ActivityType.custom, name=" ", details=" ", state="➡️ " + bot_config["prefix"] +"help")) # Rich presence
-        self.tmux_task.start() # // While
-        self.check_wallets_rooms_task.start() # // While
+        self.tmux_task.start()                # A loop cheeckup of tmux_task fonction
+        self.check_wallets_rooms_task.start() # A loop cheeckup of check_wallets_rooms_task fonction
         await log_bot_infos(client)   
 
     @tasks.loop(seconds=5)
-    async def tmux_task(self): # Execute tmux checkup every 5 seconds
+    async def tmux_task(self):
+        """
+        Execute tmux checkup every 5 seconds, if a tmux configured session 
+        in configs/tmux_config.json is not on tmux list-session (not running) and
+        start it if needed.
+        """
         await tmux(self, None, ['checkup'])
 
     @tasks.loop(seconds=5)
-    async def check_wallets_rooms_task(self): # Execute wallets rooms checkup every 5 seconds
+    async def check_wallets_rooms_task(self): 
+        """
+        Checks that the rooms in the roomscategid category configured in configs/bot_config exist for each wallet 
+        in the table wallets in database smartswap and assigns permissions to each user with clients_wallets 
+        and for each client from the clients table (userdiscordid).
+        """
         await check_wallets_rooms(self, int(bot_config["roomscategid"]))
 
-    async def on_message(self, message): # When a message is sent
+    async def on_message(self, message): 
+        """
+        Toggled at every message sent
+        """
         if message.author.id == self.user.id: return  # No respond to itself
+        
         # Imperative security that make the bot interract only on the configured discord channel (usage and logs channels)
-        if str(message.guild.id) != bot_config["discordid"]: return  # Security to only use bot in a room
+        if str(message.guild.id) != bot_config["discordid"]: return  
+
         content = message.content[len(bot_config["prefix"]):].split() # Separate command and arguments
         if not content: return # Security if there is no command and only prefix
+
         if message.content[:len(bot_config["prefix"])] != bot_config["prefix"]: return # Not good prefix command
+
         command = content[0] 
         args = content[1:]
+
         if command in admin_commands: # Verify that the command sent is in commands list
             if message.author.guild_permissions.administrator: # Admin permission security to use the bot
-                if message.channel.name == "smartswap" or str(message.channel.id) == bot_config["logschannelid"] :
+                if message.channel.name == "smartswap" or str(message.channel.id) == bot_config["logschannelid"] : # channel must be smartswap or the log channel
                     print("admin-Command: " + bot_config["prefix"] + command + " | Author: " + str(message.author) + "(" + str(message.author.id) + ") | Channel: " + str(message.channel.name))
                     await admin_commands[command](self, message, args) 
             else:
